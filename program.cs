dc3b6db2fdec3569f8cbe546b0c2f8a7d9083382
Shohruh Mahmudov <shohruhmahmudov2002@gmail.com>
2024-11-21 14:18:20 +0500

    I added program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+namespace dars_10;
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("hello world");
+        if(true)
+            Console.WriteLine("Hello");
+        else
+            Console.WriteLine("Bye");
+        for(int i = 0; i < 10; i++){
+            System.Console.WriteLine(i);
+        }
+    }
+}
